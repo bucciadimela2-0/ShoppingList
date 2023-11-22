b50a24f09b9decef31eb2951c09d33dc19489b85
Martina B <138995848+bucciadimela2-0@users.noreply.github.com>
2023-11-22 21:56:33 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kepler/CLionProjects/untitled4
-# Build directory: /Users/kepler/CLionProjects/untitled4/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test]=] "/Users/kepler/CLionProjects/untitled4/cmake-build-debug/run_tests")
-set_tests_properties([=[test]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/kepler/CLionProjects/untitled4/CMakeLists.txt;52;add_test;/Users/kepler/CLionProjects/untitled4/CMakeLists.txt;0;")
